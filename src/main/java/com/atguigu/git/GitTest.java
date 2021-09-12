1cf5242cd39dae8ce15483cd624e8adf3a629a11
lzz <17389701593@163.com>
2021-09-12 19:22:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @authon lzz
+ * @create 2021-09-12-19:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
